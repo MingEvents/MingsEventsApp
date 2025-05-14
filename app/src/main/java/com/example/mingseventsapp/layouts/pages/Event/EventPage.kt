458464b198042266ca7edae0a05cbfc3a5d94e2c
Aimadpro <aaisad2324@politecnics.barcelona>
2025-05-14 20:08:20 +0200

    modal read more in progress

--- app/src/main/java/com/example/mingseventsapp/layouts/pages/Event/EventPage.kt
+++ app/src/main/java/com/example/mingseventsapp/layouts/pages/Event/EventPage.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -37,8 +39,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.window.Dialog
 import coil.compose.AsyncImage
 import com.example.mingseventsapp.model.event.Event
+import java.io.File
 
 
 @Preview(showBackground = true)
@@ -183,6 +188,11 @@ fun EventPage() {
 
 @Composable
 fun EventItem(event: Event) {
+    var showModal by remember { mutableStateOf(false) }
+
+    if (showModal) {
+        ReadMoreModal(onClose = { showModal = false }, event)
+    }
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -242,7 +252,9 @@ fun EventItem(event: Event) {
                 }
                 Button(
 
-                    onClick = {/* Abrir pagina mas info*/},
+                    onClick = {
+                        showModal = true
+                              },
                     colors = ButtonDefaults.buttonColors(
                         containerColor = Color(0xFF14296F),
                         contentColor = Color.White
@@ -273,6 +285,42 @@ fun EventItem(event: Event) {
                           )
             }
          }
+    }
+}
+
+@Composable
+fun ReadMoreModal(onClose: () -> Unit, event: Event) {
+ //   val context = LocalContext.current
+    // val file = File(context.filesDir, event.photo)
+    Dialog(
+        onDismissRequest = { onClose() }
+          ) {
+        Box(
+            modifier = Modifier
+                .height(600.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(15.dp))
+                .background(Color(0xFFD7E9FC))
+           ) {
+            Box(
+                modifier = Modifier
+                    .width(300.dp)
+                    .height(200.dp)
+                    .clip(RoundedCornerShape(15.dp))
+                    .align(Alignment.TopCenter)
+                    .padding(top = 10.dp)
+               ) {
+                AsyncImage(
+                    model = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnS_M_rDC7mdZq0b-dRC_pWRHkxxGrbv85MA&s",
+                    contentDescription = "Imagen del evento",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(15.dp)) // ← este es clave
+                          )
+            }
+
+        }
     }
 }
 
